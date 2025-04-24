e88bf5f1d2e7e3fbe66cd1c6ebdf5092b8606abb
DzKInShO2 <dzkinsho2@protonmail.com>
2025-04-25 06:14:23 +0800

    Base UI

--- widget.py
+++ widget.py
@@ -1,2 +1,20 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow
+)
+
+
+class MynWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("app.ui", self)
+
+
 if __name__ == "__main__":
-    pass
+    app = QApplication([])
+
+    win = MynWindow()
+    win.show()
+
+    app.exec()
