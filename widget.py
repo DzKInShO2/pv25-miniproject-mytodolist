402fc6dc9dc1f303957e118d577f388ac9b29674
DzKInShO2 <dzkinsho2@protonmail.com>
2025-04-26 10:59:57 +0800

    Uhmm

--- widget.py
+++ widget.py
@@ -1,7 +1,8 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import (
     QApplication,
-    QMainWindow
+    QMainWindow,
+    QMessageBox
 )
 
 
@@ -10,11 +11,26 @@ class MynWindow(QMainWindow):
         super().__init__()
         uic.loadUi("app.ui", self)
 
+        self.__createComponents()
+        self.__setupCallbacks()
+
+    def __createComponents(self):
+        self.aboutBox = QMessageBox()
+        self.aboutBox.setWindowTitle("About Application")
+        self.aboutBox.setIcon(QMessageBox.Icon.Information)
+        self.aboutBox.setStandardButtons(QMessageBox.StandardButton.Close)
+
+    def __setupCallbacks(self):
+        self.actionAbout.triggered.connect(lambda: self.__aboutMessage())
+        self.actionExit.triggered.connect(lambda: self.close())
+
+    def __aboutMessage(self):
+        self.aboutBox.exec()
+
 
 if __name__ == "__main__":
     app = QApplication([])
 
     win = MynWindow()
     win.show()
-
     app.exec()
