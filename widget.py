798ea755b67bdcf3ba4041643dfbf9b7ad026f03
DzKInShO2 <dzkinsho2@protonmail.com>
2025-04-26 17:57:09 +0800

    add "Add Task" and "Delete Task"

--- widget.py
+++ widget.py
@@ -1,7 +1,10 @@
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
+    QInputDialog,
+    QListWidgetItem
 )
 
 
@@ -11,16 +14,35 @@ class MynWindow(QMainWindow):
         uic.loadUi("ui/app.ui", self)
 
         self._createComponents()
+        self._configureComponents()
         self._setupCallbacks()
 
     def _createComponents(self):
         pass
+
+    def _configureComponents(self):
+        self.deleteTaskButton.hide()
 
     def _setupCallbacks(self):
         self.actionExit.triggered.connect(lambda: self.close())
 
         self.taskSection.toggled.connect(self._onSectionChanged)
         self.reminderSection.toggled.connect(self._onSectionChanged)
+
+        self.newTaskButton.pressed.connect(self._onNewTaskPressed)
+
+        self.taskList.itemSelectionChanged.connect(self._onTaskSelectionChange)
+        self.deleteTaskButton.pressed.connect(self._onDeleteTaskButtonPressed)
+
+    def _onNewTaskPressed(self):
+        task, ok = QInputDialog().getText(self, "New Task", "Task: ")
+
+        if ok:
+            item = QListWidgetItem(task)
+            item.setFlags(item.flags() | Qt.ItemIsUserCheckable)
+            item.setCheckState(Qt.Unchecked)
+
+            self.taskList.addItem(item)
 
     def _onSectionChanged(self, checked):
         sender = self.sender()
@@ -30,6 +52,20 @@ class MynWindow(QMainWindow):
                 self.contentStack.setCurrentIndex(0)
             elif sender is self.reminderSection:
                 self.contentStack.setCurrentIndex(1)
+
+    def _onTaskSelectionChange(self):
+        if not self.taskList.selectedItems():
+            return
+
+        self.deleteTaskButton.show()
+
+    def _onDeleteTaskButtonPressed(self):
+        items = self.taskList.selectedItems()
+        for item in items:
+            self.taskList.takeItem(self.taskList.row(item))
+
+        self.deleteTaskButton.hide()
+        self.taskList.clearSelection()
 
 
 if __name__ == "__main__":
